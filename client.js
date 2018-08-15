5593c92998c70416ae8615d9a083a7c9ed0b2012
SavageSides <42398425+SavageSides@users.noreply.github.com>
2018-08-14 22:46:49 -0500

    Create client.js

---
+++ client.js
@@ -0,0 +1,9 @@
+import discord
+from discord.ext import commands
+import random
+
+
+TOKEN = 'NDc5MDM4NjAwODgxNjM1MzU0.DlTbGA.GGTVwCRVT2LDrsA8PxbQM7spmf8'
+
+
+client = commands.Bot(command_prefix='?')
